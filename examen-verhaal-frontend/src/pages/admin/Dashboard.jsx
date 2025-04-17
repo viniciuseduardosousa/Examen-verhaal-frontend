80e5bda05dba9927893068bdbdae3d1cdba4e9d4
Uday Singh <33316@ma-web.nl>
2025-04-17 16:26:19 +0200

    got rid of dubble width warning

--- examen-verhaal-frontend/src/pages/admin/Dashboard.jsx
+++ examen-verhaal-frontend/src/pages/admin/Dashboard.jsx
@@ -177,7 +177,7 @@ const Dashboard = () => {
                       animate={{ opacity: 1, x: 0 }}
                       exit={{ opacity: 0, x: 100 }}
                       transition={{ duration: 0.3 }}
-                      className="w-full ml-auto w-64"
+                      className="w-full ml-auto"
                     >
                       <input
                         type="text"
