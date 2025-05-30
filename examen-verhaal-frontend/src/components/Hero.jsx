f91c63af93a0f55a057950c0c19c15090aa4c318
Uday Singh <33316@ma-web.nl>
2025-05-30 11:26:22 +0200

    centered hero text tagline

--- examen-verhaal-frontend/src/components/Hero.jsx
+++ examen-verhaal-frontend/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = ({ subtitle }) => {
     <div className="w-full bg-[#FFFFF5] pt-32 pb-4">
       <div className="container mx-auto px-8">
         <div className="max-w-4xl mx-auto">
-          <div className="text-2xl font-medium text-gray-800 text-left">
+          <div className="text-2xl font-medium text-gray-800 text-center">
             <RichTextDisplay content={subtitle} />
           </div>
         </div>
